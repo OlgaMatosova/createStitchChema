8311766fe557f28bc23d194ec7d21b24c8a781a2
omatosova <matosovaolga@gmail.com>
2017-01-20 15:19:58 +0200

    added gitignor, gulpfile, package.json

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
+	watch = require('gulp-watch'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename');
+
+// Lint Task
+
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('public'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'));
+});
+  gulp.task('startLocalhost', function () {
+    connect.server({
+      livereload: true
+    });
+  });
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default Task
+gulp.task('default', [ 'sass', 'scripts', 'watch','startLocalhost']);
